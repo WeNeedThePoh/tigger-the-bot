53c712c329878352fb34c5d4b89b3fd5e2522800
WeNeedThePoh <pedromealha@hotmail.com>
2018-11-04 11:02:25 +0000

    add logic to check for a winner

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 class Game:
     ROW_DIFF = 22
     COLUMN_DIFF = 6
+    row_matches = ["", "", ""]
+    column_matches = ["", "", ""]
+    diagonal_matches = ["", ""]
 
     def __init__(self):
         self.player1 = ""
@@ -17,16 +20,41 @@ class Game:
         self.turn = player1.id
         self.reset_board()
 
+    def player_name(self, player):
+        if player == 1:
+            return self.player1.name.split("#")[0]
+        else:
+            return self.player2.name.split("#")[0]
+
     def make_visual_board(self):
         initial = 23
+        self.row_matches = ["", "", ""]
+        self.column_matches = ["", "", ""]
+        self.diagonal_matches = ["", ""]
         board = list(self.board_visual)
-        for index, row in enumerate(self.board):
-            if (index > 0):
+        for index_r, row in enumerate(self.board):
+            if index_r > 0:
                 initial = initial + self.ROW_DIFF
-            for column in row:
+            for index_c, column in enumerate(row):
                 board[initial] = column
                 initial = initial + self.COLUMN_DIFF
+                self.row_matches[index_r] += column.strip()
+                self.column_matches[index_c] += column.strip()
+                if index_r - index_c == 0:
+                    self.diagonal_matches[0] += column.strip()
+                elif index_r + index_c == 2:
+                    self.diagonal_matches[1] += column.strip()
         self.board_visual = "".join(board)
+
+    def check_winner(self):
+        if "XXX" in self.row_matches or "XXX" in self.column_matches or "XXX" in self.diagonal_matches:
+            self.state = False
+            return self.player1
+        if "OOO" in self.row_matches or "OOO" in self.column_matches or "OOO" in self.diagonal_matches:
+            self.state = False
+            return self.player2
+        else:
+            return None
 
     def reset_board(self):
         self.board = [
--- main_bot.py
+++ main_bot.py
@@ -22,10 +22,9 @@ async def on_message(message):
 
     if "!3lines" in msg_content and game.state is False:
         game.start(message.author, message.mentions[0])
-        await channel.send(f"{game.player1.nick}: X vs {game.player2.nick}: O")
+        await channel.send(f"{game.player_name(1)}: X vs {game.player_name(2)}: O")
         msg = await channel.send(f"```{game.board_visual}```")
-        turn = await channel.send(f"{game.player1.nick} your turn!")
-
+        turn = await channel.send(f"{game.player_name(1)} your turn!")
     elif game.state is True and len(msg_content) == 3 and message.author.id == game.turn:
         move = list(msg_content[1:])
         row = int(move[0])
@@ -36,14 +35,17 @@ async def on_message(message):
                 if game.player1.id == message.author.id:
                     game.board[row][column] = "X"
                     game.turn = game.player2.id
-                    game.make_visual_board()
-                    await turn.edit(content=f"{game.player2.nick} your turn!")
                 elif game.player2.id == message.author.id:
                     game.board[row][column] = "O"
                     game.turn = game.player1.id
-                    game.make_visual_board()
-                    await turn.edit(content=f"{game.player1.nick} your turn!")
 
+                game.make_visual_board()
                 await msg.edit(content=f"```{game.board_visual}```")
+                winner = game.check_winner()
+                if winner is not None:
+                    await turn.edit(content=f"{winner.nick} Won the game!")
+                else:
+                    player_name = client.get_user(game.turn).name.split("#")[0]
+                    await turn.edit(content=f"{player_name} your turn!")
 
 client.run(token)
